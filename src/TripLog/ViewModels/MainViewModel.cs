1519a03317d0844e3db193433c6ed1b117ac18f9
MarcCDJ <7518360+MarcCDJ@users.noreply.github.com>
2020-07-10 09:16:53 -0400

    feat: Add pull to refresh list.

--- src/TripLog/ViewModels/MainViewModel.cs
+++ src/TripLog/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using TripLog.Models;
 using TripLog.Services;
 using Xamarin.Forms;
@@ -20,12 +21,16 @@ namespace TripLog.ViewModels
         }
 
         public Command<TripLogEntry> ViewCommand =>
-        new Command<TripLogEntry>(async entry =>
-            await NavService.NavigateTo<DetailViewModel, TripLogEntry>(entry).ConfigureAwait(false));
+            new Command<TripLogEntry>(async entry =>
+                await NavService.NavigateTo<DetailViewModel, TripLogEntry>(entry).ConfigureAwait(false));
 
         public Command NewCommand =>
             new Command(async () =>
                 await NavService.NavigateTo<NewEntryViewModel>().ConfigureAwait(false));
+
+        Command _refreshCommand;
+        public Command RefreshCommand =>
+            _refreshCommand ?? (_refreshCommand = new Command(LoadEntries));
 
         public override void Init()
         {
@@ -40,43 +45,59 @@ namespace TripLog.ViewModels
 
         void LoadEntries()
         {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            IsBusy = true;
+
             LogEntries.Clear();
-            LogEntries.Add(new TripLogEntry
-            {
-                Title = "Washington Monument",
-                Notes = "Amazing!",
-                Rating = 3,
-                Date = new DateTime(2019, 2, 5),
-                Latitude = 38.8895,
-                Longitude = -77.0352
-            });
-            LogEntries.Add(new TripLogEntry
-            {
-                Title = "Statue of Liberty",
-                Notes = "Inspiring!",
-                Rating = 4,
-                Date = new DateTime(2019, 4, 13),
-                Latitude = 40.6892,
-                Longitude = -74.0444
-            });
-            LogEntries.Add(new TripLogEntry
-            {
-                Title = "Golden Gate Bridge",
-                Notes = "Foggy, but beautiful.",
-                Rating = 5,
-                Date = new DateTime(2019, 4, 26),
-                Latitude = 37.8268,
-                Longitude = -122.4798
-            });
-            LogEntries.Add(new TripLogEntry
-            {
-                Title = "Pikes Peak",
-                Notes = "Majestic...",
-                Rating = 5,
-                Date = new DateTime(2009, 5, 25),
-                Latitude = 38.841672,
-                Longitude = -105.042302
-            });
+
+            Task.Delay(3000).ContinueWith(_ => Device.BeginInvokeOnMainThread(() =>
+              {
+                  LogEntries.Add(new TripLogEntry
+                  {
+                      Title = "Washington Monument",
+                      Notes = "Amazing!",
+                      Rating = 3,
+                      Date = new DateTime(2019, 2, 5),
+                      Latitude = 38.8895,
+                      Longitude = -77.0352
+                  });
+
+                  LogEntries.Add(new TripLogEntry
+                  {
+                      Title = "Statue of Liberty",
+                      Notes = "Inspiring!",
+                      Rating = 4,
+                      Date = new DateTime(2019, 4, 13),
+                      Latitude = 40.6892,
+                      Longitude = -74.0444
+                  });
+
+                  LogEntries.Add(new TripLogEntry
+                  {
+                      Title = "Golden Gate Bridge",
+                      Notes = "Foggy, but beautiful.",
+                      Rating = 5,
+                      Date = new DateTime(2019, 4, 26),
+                      Latitude = 37.8268,
+                      Longitude = -122.4798
+                  });
+
+                  LogEntries.Add(new TripLogEntry
+                  {
+                      Title = "Pikes Peak",
+                      Notes = "Majestic...",
+                      Rating = 5,
+                      Date = new DateTime(2009, 5, 25),
+                      Latitude = 38.841672,
+                      Longitude = -105.042302
+                  });
+
+                  IsBusy = false;
+              }), TaskScheduler.Default);
         }
     }
 }
